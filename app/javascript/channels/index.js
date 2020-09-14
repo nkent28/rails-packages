2e8d5b82d9a1acf6a555f6a5e99c69ffe197837b
Nadia Kent nkent28@gmail.com <nkent28@gmail.com>
2020-09-15 10:20:02 +1200

    add

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,3 +3,45 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
+
+// const element = document.querySelector(CSS_SELECTOR);
+const button = document.querySelector("#click-me");
+
+// button.addEventListener(EVENT_TYPE, CALLBACK)
+button.addEventListener('click', (event) => {
+  document.body.style.backgroundColor = "blue";
+});
+
+// calling an api
+const results = document.querySelector('#results');
+
+const searchMovies = (keyword) => {
+const apiUrl = `http://www.omdbapi.com/?s=${keyword}&apikey=adf1f2d7`; //keyword is a search param
+fetch(apiUrl) //request the api
+  .then(response => response.json()) //parsed the json
+  .then((data) => {
+    // console.log(data);
+    data.Search.forEach((movie) => { //iterated over the array of movies and created strings
+      const newMovie = `<li>
+      <img src="${movie.Poster}" />
+      <p>${movie.Title}</p>
+      </li>`
+      results.insertAdjacentHTML('beforeend', newMovie) //data.Search is the key to call, the api is limiting the results to 10
+    // inserted each movie to the list, to the end of the list
+    });
+  });
+};
+
+  const searchForm = document.querySelector('#search-movies')
+    searchForm.addEventListener('submit', (event) => {
+      event.preventDefault();
+      results.innerHTML = ""; //gets rid of all the listed items so is all new information
+      const keyword = document.querySelector('#keyword').value;
+      //console.log(keywordInput);
+      searchMovies(keyword);
+    });
+
+    searchMovies("Wonder Woman");
+
+
+
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "open-uri"
+
+Booking.destroy_all
+Product.destroy_all
+
+
+puts "creating products"
+file = URI.open('https://res.cloudinary.com/dmwa29qvx/image/upload/v1581174946/fake_bouquet_q4rugt.jpg')
+product = Product.new(name: 'Wedding Bouquet', description: "Beautiful wedding bouquet(fake) for hire", price: 15, user: User.first)
+product.photos.attach(io: file, filename: 'fake_bouquet.jpg', content_type: 'image/jpg')
+product.save!
